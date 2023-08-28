0934348255367bc61880dc61604195e67635346c
Garbi93 <iseung2093@gmail.com>
2023-08-28 15:33:42 +0900

    객체와 클래스

---
+++ src/ch05reference/C12instance.java
@@ -0,0 +1,12 @@
+package ch05reference;
+
+public class C12instance {
+    public static void main(String[] args) {
+        // instance
+        // object
+        // 객체 :
+        // 1. 속성(값, 필드, property, attribute) - 냉장고, 자동차
+        // 2. 기능(메소드, 함수, 액션) - 끄다, 달리다
+    }
+
+}
