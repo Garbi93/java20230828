d64262c8224604e6fc003b5c1674406b6e10bb9b
Garbi93 <iseung2093@gmail.com>
2023-08-29 12:41:58 +0900

    8/29일 오버라이딩 연습

--- src/ch07extends/book/sec04/exam01/ComputerExample.java
+++ src/ch07extends/book/sec04/exam01/ComputerExample.java
@@ -4,10 +4,12 @@ public class ComputerExample {
     public static void main(String[] args) {
         int r = 10;
 
+        // 오버라이딩 전 것을 갖고 오는것
         Calculator calculator = new Calculator();
         System.out.println("원 면적 : " + calculator.areaCircle(r) );
         System.out.println();
 
+        // 오버라이딩 한것을 갖고 오는것
         Computer computer = new Computer();
         System.out.println("원 면적 : " + computer.areaCircle(r));
     }
