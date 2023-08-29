3b0ee7e29d22487eaf8f7a3c84ee06c0031e4b6c
Garbi93 <iseung2093@gmail.com>
2023-08-29 10:23:57 +0900

    8/29일 스테틱멤버 인스턴스멤버 간의 사용 범위

---
+++ src/ch06class/C09static.java
@@ -0,0 +1,29 @@
+package ch06class;
+
+public class C09static {
+    public static void main(String[] args) {
+
+    }
+}
+
+class MyClass09 {
+    String name;    // instance field
+    static String model; // static field
+
+    // instance method
+    void printName () {
+        // instance member끼리 접근 가능
+        System.out.println("name = " + name);
+
+        // instance member 에서 static member 접근 가능
+        System.out.println("model = " + model);
+    }
+
+    // static method
+    static void printModel () {
+        // static member 끼리 접근 가능
+        System.out.println("model = " + model);
+        // static mamber 에서 instance member에 는 접근 불가능
+        //System.out.println("name = " + name);
+    }
+}
