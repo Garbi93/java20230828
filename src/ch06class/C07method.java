3ac6eb902bd3828274f50723e60a2236e74d324b
Garbi93 <iseung2093@gmail.com>
2023-08-29 09:47:08 +0900

    8/29일 인스턴스 메소드

---
+++ src/ch06class/C07method.java
@@ -0,0 +1,37 @@
+package ch06class;
+
+public class C07method {
+    public static void main(String[] args) {
+        MyClass07 o1 = new MyClass07();
+        MyClass07 o2 = new MyClass07();
+
+        o1.myMethod1();
+        o2.myMethod1();
+        System.out.println("------------------------");
+
+        // 줄맞춤 ctrl + clt + l
+        o1.name = "java";
+        o2.name = "spring";
+
+        o1.printName();
+        o2.printName();
+        System.out.println("------------------------");
+
+    }
+}
+
+class MyClass07 {
+    //field
+    String name;
+
+
+    //method
+    void printName() {
+        System.out.println("name = " + name);
+    }
+
+    void myMethod1() {
+        System.out.println("myMethod1 실행됨");
+        System.out.println("MyClass07.myMethod1 실행됨");      // soutm
+    }
+}
