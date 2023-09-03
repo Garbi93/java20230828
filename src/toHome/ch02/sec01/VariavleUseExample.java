c061237502ee3f039445a3263e50bb5a19bc4557
Garbi93 <iseung2093@gmail.com>
2023-09-04 08:43:33 +0900

    8/31 자습

---
+++ src/toHome/ch02/sec01/VariavleUseExample.java
@@ -0,0 +1,9 @@
+package toHome.ch02.sec01;
+
+public class VariavleUseExample {
+    public static void main(String[] args) {
+        int hour = 3;
+        int minute = 5;
+        System.out.println(hour + "시간" + minute + "분");
+    }
+}
