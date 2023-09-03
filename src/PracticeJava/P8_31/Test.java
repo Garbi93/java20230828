1e239d388eb9916ea11babcf4dddb311c3901afe
Garbi93 <iseung2093@gmail.com>
2023-09-04 08:43:17 +0900

    8/31 메소드내부 클래스

--- src/PracticeJava/P8_31/Test.java
+++ src/PracticeJava/P8_31/Test.java
@@ -1,17 +1,19 @@
 package PracticeJava.P8_31;
 
-
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a;
-        int b;
-        while(true) {
-            a = sc.nextInt();
-            b = sc.nextInt();
-            System.out.println(a+b);
-        }
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int c = b/100;
+        int d = (b/10)%10;
+        int e = (b%10);
+        System.out.println(a*e);
+        System.out.println(a*d);
+        System.out.println(a*c);
+        System.out.println(a*b);
     }
 }
