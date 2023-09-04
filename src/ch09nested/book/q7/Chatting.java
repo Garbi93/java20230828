b551057ab681beea8358e5ec6ff1f939adbcda7b
Garbi93 <iseung2093@gmail.com>
2023-09-04 11:46:00 +0900

    9/04 16장 람다

---
+++ src/ch09nested/book/q7/Chatting.java
@@ -0,0 +1,7 @@
+package ch09nested.book.q7;
+
+public class Chatting {
+    class Chat {
+
+    }
+}
---
+++ src/ch16lambda/book/sec01/Calculable.java
@@ -0,0 +1,5 @@
+package ch16lambda.book.sec01;
+
+public interface Calculable {
+    void caclulate(int x, int y);
+}
