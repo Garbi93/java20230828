3c9d67b05679a2062eac2a438a503a9ab21f28e4
Garbi93 <iseung2093@gmail.com>
2023-09-05 14:17:18 +0900

    9/05 12장 hashSet을 이용 한 동등비교

---
+++ src/ch12api/lecture/C10set.java
@@ -0,0 +1,21 @@
+package ch12api.lecture;
+
+import java.util.HashSet;
+
+public class C10set {
+    public static void main(String[] args) {
+        // set : 집합
+        HashSet set = new HashSet();
+        set.add(new Object());
+        set.add(new Object());
+
+        System.out.println(set.size());
+
+        set.add(new String("java"));
+        System.out.println(set.size());
+
+        set.add(new String("java"));    // add 메소드는 같은 객체가 없을 때 만 들어간다.
+
+        System.out.println(set.size());
+    }
+}
