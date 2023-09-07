59f01764b0123f2b1a008297fe85f4f12dda01fa
Garbi93 <iseung2093@gmail.com>
2023-09-07 17:47:15 +0900

    9/07 알고리즘 연습

--- src/Algorithms/test/Test.java
+++ src/Algorithms/test/Test.java
@@ -5,21 +5,24 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();    // 총 바구니 수
-        int m = sc.nextInt();    // 도현이가 공을 던질 횟수
-        int[] A = new int[n];    // 총 바구니 수만큼 값이 들어감
-
-        for(int p=0; p<m; p++) {
-            // 바구니 수만큼 i,j,k 값을 읽는다.
-            int i = sc.nextInt();                   // k번이 적힌 공이 들어갈 첫 바구니
-            int j = sc.nextInt();                   // k번이 적힌 공이 들어갈 마지막 바구니
-            int k = sc.nextInt();                   // 공에 적힌 번호
-            for(int q=i-1; q<j; q++){               // i 번부터 j 번까지 의 바구니에 k를 넣을것
-                A[p] = k;                           // A[p]배열마다 k값을 덮어 씌운다
+        int[] A = new int[28];
+        int[] B = new int[30];
+        int n;
+        while (true) {
+            for(int i = 0; i< 30; i++) {
+                B[i] = i+1;
+                n = sc.nextInt();
+                A[i] = n;
             }
+            for(int i = 0; i< 30; i++) {
+                for(int j=0; j<30; j++){
+                    if(B[i] != A[i]){
+                        System.out.println(A[i]);
+                    }
+                }
+            }
+
         }
-        for(int p = 0; p < m; p++) {
-            System.out.println(A[p]);
-        }
+
     }
 }
