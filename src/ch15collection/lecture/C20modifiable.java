86c1c7d17dcfeff7feaed3e1e84f458d59db97f7
Garbi93 <iseung2093@gmail.com>
2023-09-12 11:32:39 +0900

    9/12 수정불가 컬렉션, 수정가능하게 변경

--- src/ch15collection/lecture/C20modifiable.java
+++ src/ch15collection/lecture/C20modifiable.java
@@ -5,10 +5,10 @@ import java.util.Set;
 
 public class C20modifiable {
     public static void main(String[] args) {
-        Set<String> set = Set.of("java", "spring", " react");
+        Set<String> set = Set.of("java", "spring", "react");
 
         Set<String> set2 = new HashSet<>(set);
-        set2.add("heml");
+        set2.add("html");
         System.out.println("set2 = " + set2);
         System.out.println("프로그램 실행중");
     }
---
+++ src/ch15collection/lecture/C21modifiable.java
@@ -0,0 +1,15 @@
+package ch15collection.lecture;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class C21modifiable {
+    public static void main(String[] args) {
+        Map<String, Integer> map = Map.of("흥민", 7, "강인", 10,"민재",30);
+        
+        Map<String, Integer> map2 = new HashMap<>(map);
+        map2.put("천수",50);
+        System.out.println("map2.size() = " + map2.size());
+        System.out.println("프로그램 실행중");
+    }
+}
