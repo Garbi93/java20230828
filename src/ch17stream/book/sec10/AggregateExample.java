1d80d7ba371a14b76a57281d5fceeeb7c84a7309
Garbi93 <iseung2093@gmail.com>
2023-09-13 09:53:23 +0900

    9/13 Optional Stream 예제

---
+++ src/ch17stream/book/sec10/AggregateExample.java
@@ -0,0 +1,51 @@
+package ch17stream.book.sec10;
+
+import java.util.Arrays;
+
+public class AggregateExample {
+    public static void main(String[] args) {
+        // 정수 배열
+        int[] arr = {1,2,3,4,5};
+
+        // 카운팅
+        long count = Arrays.stream(arr)
+                .filter(n-> n%2 ==0)
+                .count();
+        System.out.println("2의 배수 개수 = " + count);
+
+
+        // 총합
+        long sum = Arrays.stream(arr)
+                .filter(n -> n % 2 == 0)
+                .sum();
+        System.out.println("2의 배수의 총합 = " + sum);
+
+        // 평균
+        double avg = Arrays.stream(arr)
+                .filter(n -> n % 2 == 0)
+                .average()
+                .getAsDouble();
+        System.out.println("2의배수의 평균 = " + avg);
+
+        // 최대값
+        int max = Arrays.stream(arr)
+                .filter(n -> n % 2 == 0)
+                .max()
+                .getAsInt();
+        System.out.println("max = " + max);
+
+        // 최소값
+        int min = Arrays.stream(arr)
+                .filter(w -> w % 2 == 0)
+                .min()
+                .getAsInt();
+        System.out.println("min = " + min);
+
+        // 첫번째 요소
+        int first = Arrays.stream(arr)
+                .filter(e -> e % 3 == 0)
+                .findFirst()
+                .getAsInt();
+        System.out.println("첫번째 3의 배수 = " + first);
+    }
+}
