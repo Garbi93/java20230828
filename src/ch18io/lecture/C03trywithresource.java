cb1a08350f54a0a8ffd507176cc510ffe8069dac
Garbi93 <iseung2093@gmail.com>
2023-09-15 09:37:40 +0900

    9/15 18장데이터 입출력 trywithresource

--- src/ch18io/lecture/C03trywithresource.java
+++ src/ch18io/lecture/C03trywithresource.java
@@ -1,9 +1,7 @@
 package ch18io.lecture;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.Scanner;
 
 public class C03trywithresource {
     public void method1 () {
@@ -29,10 +27,13 @@ public class C03trywithresource {
         }
     }
 
+    // try - with -resource 문법
     // 위의 method1 을 간단하게 만든것
     public void method2 () {
         // try - with -resource 문법
         // 코드가 오류가 나든 안나든 close를 꼭 실행시켜준다.
+        // close를 안써도 자동으로 닫아진다
+        // try - with -resource의 변수의 타입은 AutoCloseable이어야 한다.
         try(OutputStream os = new FileOutputStream("");) {
             os.write(33);
             os.write(67);
@@ -41,5 +42,59 @@ public class C03trywithresource {
             throw new RuntimeException(e);
         }
     }
+
+
+    public void method3 () {
+        try (
+                FileOutputStream fos = new FileOutputStream("");
+                FileInputStream fis = new FileInputStream("");
+        ) {
+            fos.write(234);
+            fis.read();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void method4() {
+        Scanner scanner = new Scanner("");
+        try (scanner) { // 값이 한번만 할당 될때에는 변수명만 넣어도 괜찮다.(effectivly final과 같은 효과)
+            scanner.hasNext();
+            scanner.nextInt();
+        }
+    }
+
+    // 기존 방식
+    public void method5() {
+        try {
+            FileInputStream fio = new FileInputStream("");
+            FileOutputStream fos = new FileOutputStream("");
+
+            try (fio; fos;) {
+                fio.read();
+                fos.write(33);
+            }catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // 이런식의 try catch 생략은 사용 은 가는하나 권장하지 않는 방식이다.
+    public void method6() throws IOException {
+        // 안좋은 코드임
+        FileOutputStream fos = new FileOutputStream("");
+        FileInputStream fis = new FileInputStream("");
+
+        fos.write(33);
+        fis.read();
+
+        fos.close();
+        fis.close();
+    }
+
+
 
 }
