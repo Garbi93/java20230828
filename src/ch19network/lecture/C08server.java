d14fc6c01ec260fd411c91841c58a68cb0684fc1
Garbi93 <iseung2093@gmail.com>
2023-09-19 14:20:15 +0900

    9/19 네트워크 입출력 server to client

---
+++ src/ch19network/lecture/C08server.java
@@ -0,0 +1,38 @@
+package ch19network.lecture;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class C08server {
+    public static void main(String[] args) {
+        // server 가 client에게 파일 보내기
+        try{
+            ServerSocket serverSocket = new ServerSocket(5000);
+            Socket socket = serverSocket.accept();
+
+            String path = "C:/Temp/test1.jpg";
+            InputStream is = new FileInputStream(path);
+            BufferedInputStream bis = new BufferedInputStream(is);
+
+            OutputStream os = socket.getOutputStream();
+            BufferedOutputStream bos =new BufferedOutputStream(os);
+
+            try(serverSocket; socket; is;bis;os;bos) {
+                byte[] bytes = new byte[1024];
+                int len = 0;
+                while ((len = bis.read(bytes))!=-1) {
+                    bos.write(bytes,0,len);
+                }
+
+                bos.flush();
+
+                System.out.println("서버가 파일을 보냈습니다.╰(*°▽°*)╯");
+
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
